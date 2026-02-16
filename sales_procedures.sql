a8a80d6d0e745b94504e6eafe5de0ba0247cb600
TheIndomitableHumanSpiritTTHK <artjom@mihhailenko.com>
2026-02-16 16:45:14 +0200

    Refactor sales_procedures.sql with new structures and data
    Updated the SQL dump to include new procedures, table structures, and data insertions for brands, categories, products, customers, orders, and stocks.

--- sales_procedures.sql
+++ sales_procedures.sql
@@ -1,266 +1,380 @@
-CREATE DATABASE veebipoodMihhailenko;
-use veebipoodMihhailenko;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Loomise aeg: Veebr 16, 2026 kell 03:44 PL
+-- Serveri versioon: 10.4.32-MariaDB
+-- PHP versioon: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- tabeli categories loomine
-CREATE TABLE categories (
-category_id int PRIMARY KEY identity(1,1),
-category_name varchar(30) unique,
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-INSERT INTO categories(category_name)
-VALUES ('t-särk'), ('mantel'), ('pusa'), ('pintsak'), ('jope');
+--
+-- Andmebaas: `veebikood`
+--
 
-SELECT * FROM categories
+DELIMITER $$
+--
+-- Toimingud
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `kustutaBrand` (IN `id` INT)   BEGIN
+	SELECT * FROM BRANDS;
+	DELETE FROM brands WHERE brand_id=id;
+	SELECT * FROM BRANDS;
+END$$
 
---  protseduur mis lisab INSERT andmeid tabelisse categories
-CREATE PROCEDURE lisaCategory
-    @category_nimi VARCHAR(30)
-AS
-BEGIN
-    INSERT INTO categories (category_name) VALUES (@category_nimi);
+CREATE DEFINER=`root`@`localhost` PROCEDURE `kustutaCategory` (IN `id` INT)   BEGIN
     SELECT * FROM categories;
-END;
-
--- protseduuri kutse
-EXEC lisaCategory 'kingad';
-
--- protseduur mis uuendab (UPDATE) category id järgi
-CREATE PROCEDURE uuendaCategory
-    @id INT,
-    @uus_nimi VARCHAR(30)
-AS
-BEGIN
+    DELETE FROM categories WHERE category_id = id;
     SELECT * FROM categories;
-    UPDATE categories SET category_name = @uus_nimi WHERE category_id = @id;
-    SELECT * FROM categories;
-END;
+END$$
 
--- protseduuri kutse
-EXEC uuendaCategory 6, 'kostüüm';
+CREATE DEFINER=`root`@`localhost` PROCEDURE `kustutaKlient` (IN `id` INT)   BEGIN
+    SELECT * FROM customers;
+    DELETE FROM customers WHERE customer_id = id;
+    SELECT * FROM customers;
+END$$
 
--- protseduur mis kustutab (DELETE) tabelist id järgi
-CREATE PROCEDURE kustutaCategory
-    @id INT
-AS
-BEGIN
-    SELECT * FROM categories;
-    DELETE FROM categories WHERE category_id = @id;
-    SELECT * FROM categories;
-END;
-
-EXEC kustutaCategory 6;
-
--- tabeli brands loomine
-CREATE TABLE brands (
-brand_id int PRIMARY KEY identity(1,1),
-brand_name varchar(30) unique,
-);
-INSERT INTO brands (brand_name)
-VALUES ('Nike'),
-('Adidas'),
-('Puma'),
-('H&M'),
-('Zara');
-
-SELECT * FROM brands;
-
--- tabeli products loomine
-CREATE TABLE products (
-product_id int PRIMARY KEY identity(1,1),
-product_name varchar(30) NOT NULL,
-brand_id int,
-FOREIGN KEY (brand_id) REFERENCES brands(brand_id),
-category_id int,
-FOREIGN KEY (category_id) REFERENCES categories(category_id),
-model_year int,
-list_price decimal(7,2)
-);
-
-INSERT INTO products (product_name, brand_id, category_id, model_year, list_price)
-VALUES
-('logoga t-särk', 1, 1, 2020, 25.50),
-('sport t-särk', 2, 1, 2021, 22.00),
-('oversize t-särk', 5, 1, 2023, 19.99),
-
-('talvemantel', 5, 2, 2022, 89.90),
-('kergmantel', 2, 2, 2021, 69.00),
-
-('kapuutsiga pusa', 1, 3, 2024, 55.00),
-('dressipusa', 3, 3, 2022, 39.99),
-('basic pusa', 4, 3, 2023, 24.99),
-
-('must pintsak', 4, 4, 2022, 79.99),
-
-('talvejope',    1, 5, 2024, 120.00),
-('kerge jope',   3, 5, 2022, 70.00);
-
-
-SELECT * FROM brands;
-SELECT * FROM categories;
-
-SELECT * FROM products;
-
-UPDATE categories SET category_name='kingad'
-WHERE category_id=2;
---  protseduur mis lisab INSERT andmeid tabelisse brands
-CREATE PROCEDURE lisaBrand
-@brand_nimi varchar(30)
-as
-BEGIN
-	INSERT INTO brands (brand_name) VALUES (@brand_nimi);
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lisaBrand` (IN `brand_nimi` VARCHAR(30))   BEGIN
+	INSERT INTO brands (brand_name) VALUES (brand_nimi);
 	SELECT * from brands;
-END;
+END$$
 
--- protseduuri kutse
-EXEC lisaBrand 'Audi';
+CREATE DEFINER=`root`@`localhost` PROCEDURE `lisaCategory` (IN `category_nimi` VARCHAR(30))   BEGIN
+    INSERT INTO categories (category_name) VALUES (category_nimi);
+    SELECT * FROM categories;
+END$$
 
--- protseduur mis kustutab (DELETE) tabelist id järgi
-CREATE PROCEDURE kustutaBrand
-@id int
-AS
-BEGIN
-	SELECT * FROM BRANDS;
-	DELETE FROM brands WHERE brand_id=@id;
-	SELECT * FROM BRANDS;
-END;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `otsing1tahte` (IN `taht` CHAR(1))   BEGIN
+	SELECT * FROM brands WHERE brand_name LIKE CONCAT(taht,'%');
+END$$
 
--- protseduuri kutse
-EXEC kustutaBrand 6;
-
--- protseduur mis otsib 1 tähte järgi brandinimed
-CREATE PROCEDURE otsing1tahte
-@taht char(1)
-AS
-BEGIN
-	SELECT * FROM brands WHERE brand_name LIKE @taht+'%';
-END;
-
--- protseduuri kutse
-EXEC otsing1tahte 'A';
-
-
--- protseduur mis uuendab (UPDATE) brandinimi id järgi
-CREATE PROCEDURE uuendaBrand
-@id int,
-@uus_brandNimi varchar(30)
-AS
-BEGIN
-	SELECT * FROM brands;
-	UPDATE brands SET brand_name=@uus_brandNimi
-	WHERE brand_id=@id;
-	SELECT * FROM brands;
-END;
-
--- protseduuri kutse
-EXEC uuendaBrand 2, "Abibas";
-
--- tabeli customers loomine
-CREATE TABLE customers(
-customer_id int PRIMARY KEY identity(1,1),
-first_name varchar(30) NOT NULL,
-last_name varchar(30) NOT NULL,
-phone char(15),
-email varchar(50) UNIQUE,
-street varchar(50),
-city varchar(50),
-state_ varchar(50),
-zip_code char(5)
-);
-
-INSERT INTO customers(first_name, last_name, phone, email, street, city, state_, zip_code)
-VALUES
-('Mihhail', 'Mihhailenko', '+3725000001', 'mihhail@test.ee', 'Pärnu mnt 5', 'Tallinn', 'Harjumaa', '10141'),
-('Anna', 'Ivanova', '+3725000002', 'anna@test.ee', 'Narva mnt 20', 'Tallinn', 'Harjumaa', '10120'),
-('Karl', 'Tamm', '+3725000003', 'karl@test.ee', 'Lai 3', 'Tartu', 'Tartumaa', '51005');
-
-SELECT * FROM customers;
-
--- tabeli stores loomine
-CREATE TABLE stores(
-store_id int PRIMARY KEY identity(1,1),
-store_name varchar(30) NOT NULL,
-phone varchar(15),
-email varchar(50),
-street varchar(50),
-city varchar(50),
-state_ varchar(50),
-zip_code char(15)
-);
-
-INSERT INTO stores(store_name, phone, email, street, city, state_, zip_code)
-VALUES
-('Tallinn Pood', '+3725551111', 'tallinn@pood.ee', 'Tartu mnt 1', 'Tallinn', 'Harjumaa', '10115'),
-('Tartu Pood', '+3725552222', 'tartu@pood.ee', 'Riia 10', 'Tartu', 'Tartumaa', '51010');
-
-SELECT * FROM stores;
-
--- tabeli staffs loomine
-CREATE TABLE staffs(
-staff_id int PRIMARY KEY identity(1,1),
-first_name varchar(30) NOT NULL,
-last_name varchar(30) NOT NULL,
-email varchar(30) unique,
-phone char(15),
-active bit,
-store_id int,
-FOREIGN KEY (store_id) REFERENCES stores(store_id),
-manager_id int,
-);
-
-INSERT INTO staffs(first_name, last_name, email, phone, active, store_id, manager_id)
-VALUES
-('Liis',  'Seller', 'liis@pood.ee',  '+3726000002', 1, 1, 1),
-('Jaan',  'Seller', 'jaan@pood.ee',  '+3726000003', 1, 2, 1);
-
-SELECT * FROM staffs;
-
--- tabeli orders loomine
-CREATE TABLE orders(
-order_id int PRIMARY KEY identity(1,1),
-customer_id int,
-FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
-order_status varchar(30) check(order_status='töötlemisel' or order_status='valmis' or order_status='makstud'),
-order_date date,
-required_date date,
-shipped_date date,
-store_id int,
-FOREIGN KEY (store_id) REFERENCES stores(store_id),
-staff_id int,
-FOREIGN KEY (staff_id) REFERENCES staffs(staff_id)
-);
-
-INSERT INTO orders(customer_id, order_status, order_date, required_date, shipped_date, store_id, staff_id)
-VALUES
-(1, 1, '2026-02-01', '2026-02-05', '2026-02-03', 1, 2),
-(3, 1, '2026-02-03', '2026-02-08', '2026-02-07',  2, 3);
-
-SELECT * FROM orders;
-
--- tabeli order_items loomine
-CREATE TABLE order_items(
-order_id int,
-item_id int,
-PRIMARY KEY (order_id, item_id),
-product_id int,
-FOREIGN KEY (product_id) REFERENCES products(product_id),
-quantity int,
-list_price decimal(7,2),
-discount decimal(2,2)
-);
-
-
-
-SELECT * FROM order_items;
-
--- tabeli stocks loomine 
-CREATE TABLE stocks(
-store_id int,
-FOREIGN KEY (store_id) references stores(store_id),
-product_id int,
-FOREIGN KEY (product_id) references products(product_id),
-quantity int
-);
-
-
+CREATE DEFINER=`root`@`localhost` PROCEDURE `suurendadaStocks` ()   BEGIN
 SELECT * FROM stocks;
+UPDATE stocks SET quantity = quantity * 1.10;
+SELECT * FROM stocks;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `tähtajaÜletanudOrders` ()   BEGIN
+SELECT * FROM `orders` WHERE shipped_date > required_date;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `uuendaBrand` (IN `id` INT, IN `uus_brandNimi` VARCHAR(30))   BEGIN
+	SELECT * FROM brands;
+	UPDATE brands SET brand_name=uus_brandNimi
+	WHERE brand_id=id;
+	SELECT * FROM brands;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `uuendaCategory` (IN `uus_nimi` VARCHAR(30), IN `id` INT)   BEGIN
+    SELECT * FROM categories;
+    UPDATE categories SET category_name = uus_nimi WHERE category_id = id;
+    SELECT * FROM categories;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `brands`
+--
+
+CREATE TABLE `brands` (
+  `brand_id` int(11) NOT NULL,
+  `brand_name` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `brands`
+--
+
+INSERT INTO `brands` (`brand_id`, `brand_name`) VALUES
+(1, 'Adidas'),
+(2, 'H&M'),
+(3, 'Nike'),
+(4, 'Puma'),
+(5, 'Zara');
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`) VALUES
+(1, 't-särk'),
+(2, 'mantel'),
+(3, 'pusa'),
+(4, 'pintsak'),
+(5, 'jope');
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `customers`
+--
+
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `phone` char(15) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `street` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `state_` varchar(50) DEFAULT NULL,
+  `zip_code` char(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `customers`
+--
+
+INSERT INTO `customers` (`customer_id`, `first_name`, `last_name`, `phone`, `email`, `street`, `city`, `state_`, `zip_code`) VALUES
+(1, 'Mihhail', 'Mihhailenko', '+3725000001', 'mihhail@test.ee', 'Pärnu mnt 5', 'Tallinn', 'Harjumaa', '10141'),
+(2, 'Anna', 'Ivanova', '+3725000002', 'anna@test.ee', 'Narva mnt 20', 'Tallinn', 'Harjumaa', '10120'),
+(3, 'Karl', 'Tamm', '+3725000003', 'karl@test.ee', 'Lai 3', 'Tartu', 'Tartumaa', '51005'),
+(7, 'TEST', 'Mihhailenko', '+3725000001', 'TEST@test.ee', 'Pärnu mnt 5', 'Tallinn', 'Harjumaa', '10141');
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  `order_status` varchar(30) DEFAULT NULL CHECK (`order_status` = 'töötlemisel' or `order_status` = 'valmis' or `order_status` = 'makstud'),
+  `order_date` date DEFAULT NULL,
+  `required_date` date DEFAULT NULL,
+  `shipped_date` date DEFAULT NULL,
+  `store_id` int(11) DEFAULT NULL,
+  `staff_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `customer_id`, `order_status`, `order_date`, `required_date`, `shipped_date`, `store_id`, `staff_id`) VALUES
+(1, 1, 'valmis', '2026-02-01', '2026-02-05', '2026-02-03', 1, 2),
+(2, 3, 'makstud', '2026-02-03', '2026-02-08', '2026-02-07', 2, 3),
+(3, 2, 'töötlemisel', '2026-02-01', '2026-02-05', '2026-02-10', 1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(30) NOT NULL,
+  `brand_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `model_year` int(11) DEFAULT NULL,
+  `list_price` decimal(7,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_name`, `brand_id`, `category_id`, `model_year`, `list_price`) VALUES
+(1, 'logoga t-särk', 1, 1, 2020, 25.50),
+(2, 'sport t-särk', 2, 1, 2021, 22.00),
+(3, 'oversize t-särk', 5, 1, 2023, 19.99),
+(4, 'talvemantel', 5, 2, 2022, 89.90),
+(5, 'kergmantel', 2, 2, 2021, 69.00),
+(6, 'kapuutsiga pusa', 1, 3, 2024, 55.00),
+(7, 'dressipusa', 3, 3, 2022, 39.99),
+(8, 'basic pusa', 4, 3, 2023, 24.99),
+(9, 'must pintsak', 4, 4, 2022, 79.99),
+(10, 'talvejope', 1, 5, 2024, 120.00),
+(11, 'kerge jope', 3, 5, 2022, 70.00);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `stocks`
+--
+
+CREATE TABLE `stocks` (
+  `store_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `stocks`
+--
+
+INSERT INTO `stocks` (`store_id`, `product_id`, `quantity`) VALUES
+(1, 3, 17),
+(1, 7, 9),
+(2, 10, 22);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `stores`
+--
+
+CREATE TABLE `stores` (
+  `store_id` int(11) NOT NULL,
+  `store_name` varchar(30) NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `street` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `state_` varchar(50) DEFAULT NULL,
+  `zip_code` char(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Andmete tõmmistamine tabelile `stores`
+--
+
+INSERT INTO `stores` (`store_id`, `store_name`, `phone`, `email`, `street`, `city`, `state_`, `zip_code`) VALUES
+(1, 'Tallinn Pood', '+3725551111', 'tallinn@pood.ee', 'Tartu mnt 1', 'Tallinn', 'Harjumaa', '10115'),
+(2, 'Tartu Pood', '+3725552222', 'tartu@pood.ee', 'Riia 10', 'Tartu', 'Tartumaa', '51010');
+
+--
+-- Indeksid tõmmistatud tabelitele
+--
+
+--
+-- Indeksid tabelile `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`brand_id`),
+  ADD UNIQUE KEY `brand_name` (`brand_name`);
+
+--
+-- Indeksid tabelile `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- Indeksid tabelile `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indeksid tabelile `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indeksid tabelile `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `brand_id` (`brand_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indeksid tabelile `stocks`
+--
+ALTER TABLE `stocks`
+  ADD KEY `store_id` (`store_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indeksid tabelile `stores`
+--
+ALTER TABLE `stores`
+  ADD PRIMARY KEY (`store_id`);
+
+--
+-- AUTO_INCREMENT tõmmistatud tabelitele
+--
+
+--
+-- AUTO_INCREMENT tabelile `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT tabelile `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT tabelile `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT tabelile `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT tabelile `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT tabelile `stores`
+--
+ALTER TABLE `stores`
+  MODIFY `store_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Tõmmistatud tabelite piirangud
+--
+
+--
+-- Piirangud tabelile `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`);
+
+--
+-- Piirangud tabelile `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`brand_id`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
+
+--
+-- Piirangud tabelile `stocks`
+--
+ALTER TABLE `stocks`
+  ADD CONSTRAINT `stocks_ibfk_1` FOREIGN KEY (`store_id`) REFERENCES `stores` (`store_id`),
+  ADD CONSTRAINT `stocks_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
